c3faf423fb7c059682b1f33e12fadcba4cf10234
Josué Combat Serafim <jcombatserafim@gmail.com>
2022-08-24 15:15:27 -0300

    feat: add examples of admin page

---
+++ react/AdminExample.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './style.css'
+
+function AdminExample() {
+  return (
+    <div className={style.abandonedTaxContainer}>
+      <h3 className={style.abandonedTaxTitle}>AdminExample</h3>
+    </div>
+  )
+}
+
+export default AdminExample
---
+++ react/AdminExampleDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './style.css'
+
+function AdminExampleDetail() {
+  return (
+    <div className={style.abandonedTaxContainer}>
+      <h3 className={style.abandonedTaxTitle}>AdminExampleDetail</h3>
+    </div>
+  )
+}
+
+export default AdminExampleDetail
